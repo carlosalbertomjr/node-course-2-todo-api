e19dde226dc5486557e7b1f120dedd519b8d6887
Carlos Alberto Moraes Jr <carlosalberto@Carloss-MacBook-Pro.local>
2017-07-05 20:52:09 -0300

    Add delete script

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,28 @@
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB Server');
+  }
+  console.log('Connected to MongoDB server');
+
+  // deleteMany
+  // db.collection('Todos').deleteMany({text: "Eat launch"}).then((result) => {
+  //   console.log(result);
+  // }, (err) => {});
+  // deleteOne
+  // db.collection('Todos').deleteOne({text: "Walk the dog"}).then((result) => {
+  //   console.log(result);
+  // }, (err) => {
+  //   console.log(err);
+  // });
+
+  // findOneAndDelete
+  db.collection('Todos').findOneAndDelete({complete: false}).then((result) => {
+    console.log(result);
+  }, (err) => {
+    console.log(err);
+  });
+  db.close();
+});
