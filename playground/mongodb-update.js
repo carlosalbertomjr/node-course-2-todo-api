79b11ecea20ba50fbf84a0998d76fc21fac96c73
Carlos Alberto Moraes Jr <carlosalberto@Carloss-MacBook-Pro.local>
2017-07-07 20:21:24 -0300

    Add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,41 @@
+//const MongoClient = require('mongodb').MongoClient;
+const {
+  MongoClient,
+  ObjectID
+} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB Server');
+  }
+  console.log('Connected to MongoDB server');
+
+  // db.collection('Todos').findOneAndUpdate({
+  //   _id: new ObjectID('595c3404be8bc9806d92a63a')
+  // }, {
+  //   $set: {
+  //     completed: false
+  //   }
+  // }, {
+  //   returnOriginal: false
+  // }).then((result) => {
+  //   console.log(result);
+  // })
+
+  db.collection('Users').findOneAndUpdate({
+    _id: new ObjectID('595ad2379d2ff1096c8d3603')
+  }, {
+    $set: {
+      name: 'Carlos Alberto'
+    },
+    $inc: {
+      age: 1
+    }
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result);
+  });
+
+  db.close();
+});
